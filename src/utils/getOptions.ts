d3e3c9c7994213515a0ae8147859355747bbfe0b
Sudip Shrestha <sudipstha08@gmail.com>
2021-05-30 16:51:11 +0545

    :bug: fix spacing in select field labels

--- src/utils/getOptions.ts
+++ src/utils/getOptions.ts
@@ -1,9 +1,13 @@
-// getOptions gets the options from ENUM
+import _ from 'lodash'
+
+/**
+ * getOptions gets the options from ENUM
+ **/
 const getOptions = Enum => {
   const options: any = []
   for (const [key, value] of Object.entries(Enum)) {
     if (isNaN(Number(key))) {
-      options.push({ label: key, value })
+      options.push({ label: _.startCase(key), value })
     }
   }
   return options
